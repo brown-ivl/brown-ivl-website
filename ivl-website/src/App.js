d2911678dd99bf6bb5b9b77092e86c2daf3d6a54
trajnishBrown <tarun_rajnish@brown.edu>
2022-03-02 00:27:37 -0500

    navbar

---
+++ ivl-website/src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+
+import "./App.css";
+import MainNav from "./components/navbar/MainNav";
+import Main from "./components/main/Main";
+import React from "react";
+
+function App() {
+  return (
+    <Router>
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="xl">
+          <Box sx={{ bgcolor: "#EFECE5", height: "100vh" }}>
+            <MainNav />
+            <Routes>
+              <Route path="/" element={<Main />}></Route>
+            </Routes>
+          </Box>
+        </Container>
+      </React.Fragment>
+    </Router>
+  );
+}
+
+export default App;
---
+++ ivl-website/src/components/main/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Main.css";
+
+const Main = () => {
+  return <div>{/* <h1>Homepage</h1> */}</div>;
+};
+
+export default Main;
