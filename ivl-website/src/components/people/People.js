f6cf3c0ef320b0f86ce75e1a3e11c7a30673787b
trajnishBrown <tarun_rajnish@brown.edu>
2022-05-03 00:15:41 -0400

    changes to people UI

--- ivl-website/src/components/people/People.js
+++ ivl-website/src/components/people/People.js
@@ -1,298 +1,201 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+import Paper from "@mui/material/Paper";
 
 export default function RecipeReviewCard() {
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
   return (
     <div>
-      <Typography variant="h4" component="h1" align="center" marginTop={0.5}>
-        Principal Investigator
-      </Typography>
-      <Grid container justifyContent="center" alignItems="center" spacing={1}>
-        <Grid item xs={12}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-              avatar={
-                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                  R
-                </Avatar>
-              }
-              action={
-                <IconButton aria-label="settings">
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title="Shrimp and Chorizo Paella"
-              subheader="September 14, 2016"
-            />
-            <CardMedia
-              component="img"
-              height="220"
-              image="/images/people/default.png"
-              alt="Paella dish"
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to
-                cook together with your guests. Add 1 cup of frozen peas along
-                with the mussels, if you like.
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="share">
-                <ShareIcon />
-              </IconButton>
-              <ExpandMore
-                expand={expanded}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </ExpandMore>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-              <CardContent>
-                <Typography paragraph>Method:</Typography>
-                <Typography paragraph>
-                  Heat 1/2 cup of the broth in a pot until simmering, add
-                  saffron and set aside for 10 minutes.
-                </Typography>
-                <Typography paragraph>
-                  Heat oil in a (14- to 16-inch) paella pan or a large, deep
-                  skillet over medium-high heat. Add chicken, shrimp and
-                  chorizo, and cook, stirring occasionally until lightly
-                  browned, 6 to 8 minutes. Transfer shrimp to a large plate and
-                  set aside, leaving chicken and chorizo in the pan. Add
-                  pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                  pepper, and cook, stirring often until thickened and fragrant,
-                  about 10 minutes. Add saffron broth and remaining 4 1/2 cups
-                  chicken broth; bring to a boil.
-                </Typography>
-                <Typography paragraph>
-                  Add rice and stir very gently to distribute. Top with
-                  artichokes and peppers, and cook without stirring, until most
-                  of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                  medium-low, add reserved shrimp and mussels, tucking them down
-                  into the rice, and cook again without stirring, until mussels
-                  have opened and rice is just tender, 5 to 7 minutes more.
-                  (Discard any mussels that don&apos;t open.)
-                </Typography>
-                <Typography>
-                  Set aside off of the heat to let rest for 10 minutes, and then
-                  serve.
-                </Typography>
-              </CardContent>
-            </Collapse>
-          </Card>
-        </Grid>
-      </Grid>
-      <Typography variant="h4" component="h1" align="center" marginTop={0.5}>
-        PhD Students
-      </Typography>
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
+      <Paper
+        elevation={3}
+        sx={{
+          pt: 1,
+          mt: 1,
+          height: "auto",
+          bgcolor: "#F7F6F2",
+        }}
       >
-        <Grid item xs={6}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-              avatar={
-                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                  R
-                </Avatar>
-              }
-              action={
-                <IconButton aria-label="settings">
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title="Shrimp and Chorizo Paella"
-              subheader="September 14, 2016"
-            />
-            <CardMedia
-              component="img"
-              height="220"
-              image="/images/people/default.png"
-              alt="Paella dish"
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to
-                cook together with your guests. Add 1 cup of frozen peas along
-                with the mussels, if you like.
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="share">
-                <ShareIcon />
-              </IconButton>
-              <ExpandMore
-                expand={expanded}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </ExpandMore>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <Typography variant="h4" component="h1" align="center" marginTop={0.5}>
+          Principal Investigator
+        </Typography>
+        <Grid
+          container
+          spacing={1}
+          alignItems="center"
+          justifyContent="center"
+          sx={{ pl: 1, pr: 1 }}
+        >
+          <Grid item>
+            <Card sx={{ width: 250, textAlign: "center" }}>
+              <CardMedia
+                component="img"
+                image="/images/people/default.png"
+                alt="default image"
+              />
               <CardContent>
-                <Typography paragraph>Method:</Typography>
-                <Typography paragraph>
-                  Heat 1/2 cup of the broth in a pot until simmering, add
-                  saffron and set aside for 10 minutes.
+                <Typography variant="h5" component="div">
+                  Name
                 </Typography>
-                <Typography paragraph>
-                  Heat oil in a (14- to 16-inch) paella pan or a large, deep
-                  skillet over medium-high heat. Add chicken, shrimp and
-                  chorizo, and cook, stirring occasionally until lightly
-                  browned, 6 to 8 minutes. Transfer shrimp to a large plate and
-                  set aside, leaving chicken and chorizo in the pan. Add
-                  pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                  pepper, and cook, stirring often until thickened and fragrant,
-                  about 10 minutes. Add saffron broth and remaining 4 1/2 cups
-                  chicken broth; bring to a boil.
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  Some sub text
                 </Typography>
-                <Typography paragraph>
-                  Add rice and stir very gently to distribute. Top with
-                  artichokes and peppers, and cook without stirring, until most
-                  of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                  medium-low, add reserved shrimp and mussels, tucking them down
-                  into the rice, and cook again without stirring, until mussels
-                  have opened and rice is just tender, 5 to 7 minutes more.
-                  (Discard any mussels that don&apos;t open.)
-                </Typography>
-                <Typography>
-                  Set aside off of the heat to let rest for 10 minutes, and then
-                  serve.
-                </Typography>
+                <Typography variant="body2">(2018 - 2022)</Typography>
               </CardContent>
-            </Collapse>
-          </Card>
+            </Card>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-              avatar={
-                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                  R
-                </Avatar>
-              }
-              action={
-                <IconButton aria-label="settings">
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title="Shrimp and Chorizo Paella"
-              subheader="September 14, 2016"
-            />
-            <CardMedia
-              component="img"
-              height="220"
-              image="/images/people/default.png"
-              alt="Paella dish"
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to
-                cook together with your guests. Add 1 cup of frozen peas along
-                with the mussels, if you like.
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="share">
-                <ShareIcon />
-              </IconButton>
-              <ExpandMore
-                expand={expanded}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </ExpandMore>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <Typography variant="h4" component="h1" align="center" marginTop={0.5}>
+          PhD Students
+        </Typography>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+        >
+          <Grid item>
+            <Card sx={{ width: 250, textAlign: "center" }}>
+              <CardMedia
+                component="img"
+                image="/images/people/default.png"
+                alt="default image"
+              />
               <CardContent>
-                <Typography paragraph>Method:</Typography>
-                <Typography paragraph>
-                  Heat 1/2 cup of the broth in a pot until simmering, add
-                  saffron and set aside for 10 minutes.
+                <Typography variant="h5" component="div">
+                  Name
                 </Typography>
-                <Typography paragraph>
-                  Heat oil in a (14- to 16-inch) paella pan or a large, deep
-                  skillet over medium-high heat. Add chicken, shrimp and
-                  chorizo, and cook, stirring occasionally until lightly
-                  browned, 6 to 8 minutes. Transfer shrimp to a large plate and
-                  set aside, leaving chicken and chorizo in the pan. Add
-                  pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                  pepper, and cook, stirring often until thickened and fragrant,
-                  about 10 minutes. Add saffron broth and remaining 4 1/2 cups
-                  chicken broth; bring to a boil.
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  Some sub text
                 </Typography>
-                <Typography paragraph>
-                  Add rice and stir very gently to distribute. Top with
-                  artichokes and peppers, and cook without stirring, until most
-                  of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                  medium-low, add reserved shrimp and mussels, tucking them down
-                  into the rice, and cook again without stirring, until mussels
-                  have opened and rice is just tender, 5 to 7 minutes more.
-                  (Discard any mussels that don&apos;t open.)
-                </Typography>
-                <Typography>
-                  Set aside off of the heat to let rest for 10 minutes, and then
-                  serve.
-                </Typography>
+                <Typography variant="body2">(2018 - 2022)</Typography>
               </CardContent>
-            </Collapse>
-          </Card>
+            </Card>
+          </Grid>
+          <Grid item>
+            <Card sx={{ width: 250, textAlign: "center" }}>
+              <CardMedia
+                component="img"
+                image="/images/people/default.png"
+                alt="default image"
+              />
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  Name
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  Some sub text
+                </Typography>
+                <Typography variant="body2">(2018 - 2022)</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
+        <Typography variant="h4" component="h1" align="center" marginTop={0.5}>
+          Undergraduate / Masters Students
+        </Typography>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+        >
+          <Grid item>
+            <Card sx={{ width: 250, textAlign: "center" }}>
+              <CardMedia
+                component="img"
+                image="/images/people/default.png"
+                alt="default image"
+              />
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  Name
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  Some sub text
+                </Typography>
+                <Typography variant="body2">(2018 - 2022)</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item>
+            <Card sx={{ width: 250, textAlign: "center" }}>
+              <CardMedia
+                component="img"
+                image="/images/people/default.png"
+                alt="default image"
+              />
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  Name
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  Some sub text
+                </Typography>
+                <Typography variant="body2">(2018 - 2022)</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item>
+            <Card sx={{ width: 250, textAlign: "center" }}>
+              <CardMedia
+                component="img"
+                image="/images/people/default.png"
+                alt="default image"
+              />
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  Name
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  Some sub text
+                </Typography>
+                <Typography variant="body2">(2018 - 2022)</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item>
+            <Card sx={{ width: 250, textAlign: "center" }}>
+              <CardMedia
+                component="img"
+                image="/images/people/default.png"
+                alt="default image"
+              />
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  Name
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  Some sub text
+                </Typography>
+                <Typography variant="body2">(2018 - 2022)</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item>
+            <Card sx={{ width: 250, textAlign: "center" }}>
+              <CardMedia
+                component="img"
+                image="/images/people/default.png"
+                alt="default image"
+              />
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  Name
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  Some sub text
+                </Typography>
+                <Typography variant="body2">(2018 - 2022)</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Paper>
     </div>
   );
 }
