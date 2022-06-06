a177257b8cb651d97b7eff1fff9b70511adbd476
trajnishBrown <tarun_rajnish@brown.edu>
2022-06-06 18:53:19 +0530

    hashrouter

--- ivl-website/src/App.js
+++ ivl-website/src/App.js
@@ -18,7 +18,7 @@ import People from "./components/people/People";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <React.Fragment>
         <CssBaseline />
         <MainNav />
@@ -33,7 +33,7 @@ function App() {
           </Box>
         </Container>
       </React.Fragment>
-    </Router>
+    </HashRouter>
   );
 }
 
